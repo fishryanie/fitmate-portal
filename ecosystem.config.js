006de92e73b59ff89227d6e4a4baf5c019d7a320
quanphps14761 <qphanquan1998@gmail.com>
2023-10-03 23:06:49 +0700

    fix pm2

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,6 +4,16 @@ module.exports = {
       name: 'FitMate',
       namespace: 'FitMate',
       script: './dist/src/main.js',
+      autorestart: true,
+      max_restarts: 10,
+      max_memory_restart: 500000000,
+      env: {
+        NODE_ENV: 'development',
+      },
+      env_production: {
+        NODE_ENV: 'production',
+      },
+      exec_mode: 'cluster',
     },
   ],
 };
